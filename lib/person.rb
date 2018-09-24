fb3755df752b182419cc52bbabcabffa8cae4c5d
Joshua Clarence Sadsad <joshsad2@gmail.com>
2018-09-24 23:02:36 +0000

    automatically backed up by learn

--- lib/person.rb
+++ lib/person.rb
@@ -5,5 +5,5 @@ class Person
     :wrist_size, :glove_size, :pant_length, :pant_width
     
   def initialize(attributes)
-    attributes.each {|attribute, value| self.send()
+    attributes.each {|attribute, value| self.send(())
 end
